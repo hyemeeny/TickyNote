32633847c1405a1062cde40ac8e51324949adf9e
WL-HARPER\playauto\workspace <hyemeeeny@gmail.com>
2025-06-17 15:45:54 +0900

    [Test] getTodos 디버깅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { supabase } from '@/api/supabase';
 import TodoInput from '@/components/TodoInput';
 import TodoList from '@/components/TodoList';
 
-export default function Home() {
+export const getTodos = async () => {
+  const { data, error } = await supabase.from('todos').select('*');
+  if (error) throw error;
+
+  return data;
+};
+
+export default async function Home() {
+  const todos = await getTodos();
+  console.log('todos', todos);
+
   return (
     <main>
       <TodoInput />
