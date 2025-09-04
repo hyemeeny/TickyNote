090b9d6215842de4554eb229428ed91682eb4a76
WL-HARPER\playauto\workspace <hyemeeeny@gmail.com>
2025-09-04 15:23:16 +0900

    [Feat] HydrationBoudary 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 import TodoInput from '@/components/TodoInput';
 import TodoList from '@/components/TodoList';
+import { supabase } from '@/lib/supabase/client';
+
+const getTodos = async () => {
+  const { data, error } = await supabase.from('todos').select('*');
+  if (error) throw error;
+  return data;
+};
 
 export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['todos'],
+    queryFn: getTodos,
+  });
+
   return (
-    <main>
-      <TodoInput />
-      <TodoList />
-    </main>
+    // HydrationBoudary: Tanstack Query의 상태를 서버에서 클라이언트로 복원하는 데 사용되는 컴포넌트
+    // dehydrate: Tanstack Query의 queryClient 상태를 JSON으로 직렬화해 클라이언트로 전달
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <main>
+        <TodoInput />
+        <TodoList />
+      </main>
+    </HydrationBoundary>
   );
 }
