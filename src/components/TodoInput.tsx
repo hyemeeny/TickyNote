2bb48c84cc24397a6c23b8f09177f4b22c9f552f
WL-HARPER\playauto\workspace <hyemeeeny@gmail.com>
2025-09-04 16:43:33 +0900

    [Refactor] 공용 Input 컴포넌트 수정

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,56 +1,28 @@
-'use client';
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 
-import { z } from 'zod';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useCreateTodo } from '@/hooks/useTodos';
+interface TodoInputProps {
+  label: string;
+  name: string;
+  type: string;
+  placeholder?: string;
+  register: UseFormRegisterReturn;
+  errors?: FieldError;
+}
 
-const schema = z.object({
-  title: z.string().min(1, { message: '할 일을 입력해주세요!' }),
-  description: z.string().nullable(),
-});
-
-type FormData = z.infer<typeof schema>;
-
-const TodoInput = () => {
-  const createTodo = useCreateTodo();
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(schema),
-  });
-
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    createTodo.mutate(data);
-    reset();
-  };
-
+const TodoInput = ({
+  label,
+  name,
+  type,
+  placeholder,
+  register,
+  errors,
+}: TodoInputProps) => {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="todo">TODOLIST</label>
-      <div className="flex gap-1">
-        <input
-          className="border"
-          type="text"
-          id="title"
-          placeholder="할 일을 입력하세요"
-          {...register('title')}
-        />
-        <textarea
-          className="border"
-          id="description"
-          {...register('description')}
-        />
-        <button type="submit">추가</button>
-      </div>
-      {errors.title && (
-        <p className="text-red-500 text-sm">{errors.title.message}</p>
-      )}
-    </form>
+    <>
+      {label && <label htmlFor={name}>{label}</label>}
+      <input type={type} id={name} placeholder={placeholder} {...register} />
+      {errors && <p className="text-red-500 text-sm">{errors.message}</p>}
+    </>
   );
 };
 
