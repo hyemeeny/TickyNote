e266f776b46a96b756edb1976ee088c47754a731
WL-HARPER\playauto\workspace <hyemeeeny@gmail.com>
2025-12-24 16:26:55 +0900

    [Refactor] 페이지네이션 버튼 및 ui 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,33 +1,85 @@
+import { flexRowCenter } from '@/styles/mixins';
 import { PaginationProps } from '@/types/ticky';
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+  MdKeyboardDoubleArrowRight,
+} from 'react-icons/md';
+import styled, { css } from 'styled-components';
+
+const PAGE_SIZE = 5;
 
 const Pagination = ({ page, totalPage, onChange }: PaginationProps) => {
+  const start = Math.floor((page - 1) / PAGE_SIZE) * PAGE_SIZE + 1;
+  const end = Math.min(start + PAGE_SIZE - 1, totalPage);
+  const pages = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+
   return (
-    <nav aria-label="pagination">
+    <StyledPagination aria-label="pagination">
       {/* 이전 페이지 */}
-      <button onClick={() => onChange(page - 1)} disabled={page === 1}>
-        &lt;
-      </button>
+      <StyledArrowBtn
+        onClick={() => onChange(start - 1)}
+        disabled={start === 1}
+      >
+        <MdKeyboardDoubleArrowLeft />
+      </StyledArrowBtn>
+      <StyledArrowBtn onClick={() => onChange(page - 1)} disabled={page === 1}>
+        <MdKeyboardArrowLeft />
+      </StyledArrowBtn>
 
       {/* 페이지 번호 */}
-      {Array.from({ length: totalPage }).map((_, i) => {
-        const pageNum = i + 1;
+      {pages.map((num) => {
         return (
-          <button
-            key={pageNum}
-            onClick={() => onChange(pageNum)}
-            aria-current={page === pageNum}
+          <StyledNumBtn
+            key={num}
+            onClick={() => onChange(num)}
+            aria-current={num === page ? 'page' : undefined}
           >
-            {pageNum}
-          </button>
+            {num}
+          </StyledNumBtn>
         );
       })}
 
       {/* 다음 페이지 */}
-      <button onClick={() => onChange(page + 1)} disabled={page === totalPage}>
-        &gt;
-      </button>
-    </nav>
+      <StyledArrowBtn
+        onClick={() => onChange(page + 1)}
+        disabled={page === totalPage}
+      >
+        <MdKeyboardArrowRight />
+      </StyledArrowBtn>
+      <StyledArrowBtn
+        onClick={() => onChange(end + 1)}
+        disabled={end === totalPage}
+      >
+        <MdKeyboardDoubleArrowRight />
+      </StyledArrowBtn>
+    </StyledPagination>
   );
 };
 
 export default Pagination;
+
+const CommonStyles = css`
+  width: 1.875rem;
+  height: 1.875rem;
+  ${flexRowCenter}
+  border-radius: 6.25rem;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const StyledPagination = styled.nav`
+  ${flexRowCenter}
+`;
+
+const StyledArrowBtn = styled.button`
+  ${CommonStyles}
+`;
+
+const StyledNumBtn = styled.button`
+  ${CommonStyles}
+  &[aria-current='page'] {
+    background-color: ${({ theme }) => theme.colors.point};
+    cursor: default;
+  }
+`;
