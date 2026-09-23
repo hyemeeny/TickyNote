d53ce2fbcbac40fb6f15762cff9bb3cc690aa770
JIHYEMIN <hyemeeeny@gmail.com>
2026-09-23 20:25:03 +0900

    [Fix] 함수이름 proxy로 수정

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export const middleware = async (request: NextRequest) => {
+export const proxy = async (request: NextRequest) => {
   let response = NextResponse.next({ request });
 
   const supabase = createServerClient(
